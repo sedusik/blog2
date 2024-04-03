3530608f9e9f3c63b934c51ead584e22200eff44
sedusik <sedusova.tanya@yandex.ru>
2024-04-03 08:39:59 +0300

    add post updates(part 2)

--- app/Http/Controllers/Admin/Post/StoreController.php
+++ app/Http/Controllers/Admin/Post/StoreController.php
@@ -16,8 +16,12 @@ class StoreController extends Controller
             $tagIds = $data['tag_ids'];
             unset($data['tag_ids']);
 
-            $data['preview_image'] = Storage::put('/images', $data['preview_image']);
-            $data['main_image'] = Storage::put('/images', $data['main_image']);
+            if( array_key_exists('preview_image',$data)){
+                $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
+            }
+            if( array_key_exists('main_image',$data)){
+                $data['main_image'] = Storage::disk('public')->put('/images', $data['main_image']);
+            }
 
             $post = Post::firstOrCreate($data);
             $post->tags()->attach($tagIds);
--- resources/views/admin/post/edit.blade.php
+++ resources/views/admin/post/edit.blade.php
@@ -27,12 +27,12 @@
                 <!-- Small boxes (Stat box) -->
                 <div class="row">
                     <div class="col-12">
-                        <form action=" {{ route('admin.post.update', $post->id) }}" method="POST" class="w-25">
+                        <form action=" {{ route('admin.post.update', $post->id) }}" method="POST" enctype="multipart/form-data">
                             @csrf
                             @method('PATCH')
                             <div class="form-group w-25">
                                 <input type="text" class="form-control" name="title" placeholder="Название поста"
-                                       value=" {{ old('title') }}"
+                                       value=" {{ $post->title }}"
                                 >
                                 @error('title')
                                 <div class="text-danger">Это поле необходимо заполнить</div>
@@ -40,7 +40,7 @@
                             </div>
                             <div class="form-group">
                                 <textarea id="summernote" name="content">
-                                    {{ old('content') }}
+                                    {{ $post->content }}
                                 </textarea>
                                 @error('content')
                                 <div class="text-danger">Это поле необходимо заполнить</div>
@@ -48,6 +48,9 @@
                             </div>
                             <div class="form-group w-50">
                                 <label for="exampleInputFile">Добавить превью</label>
+                                <div class="w-50 mb-2">
+                                    <img src="{{ url('storage/' . $post->preview_image) }}" alt="preview_image" class="w-50">
+                                </div>
                                 <div class="input-group">
                                     <div class="custom-file">
                                         <input type="file" class="custom-file-input" name="preview_image">
@@ -63,6 +66,9 @@
                             </div>
                             <div class="form-group w-50">
                                 <label for="exampleInputFile">Добавить главное изображение</label>
+                                <div class="w-50 mb-2">
+                                    <img src="{{ url('storage/' . $post->main_image) }}" alt="main_image" class="w-50">
+                                </div>
                                 <div class="input-group">
                                     <div class="custom-file">
                                         <input type="file" class="custom-file-input" name="main_image">
@@ -81,7 +87,7 @@
                                 <select name="category_id" class="form-control">
                                     @foreach($categories as $category)
                                         <option value="{{ $category->id }}"
-                                            {{ $category->id == old('category_id') ? ' selected' : '' }}
+                                            {{ $category->id == $post->category_id ? ' selected' : '' }}
                                         >{{ $category->title }}</option>
                                     @endforeach
                                 </select>
@@ -91,7 +97,7 @@
                                 <div class="select2-purple">
                                     <select class="select2" name="tag_ids[]" multiple="multiple" data-placeholder="Выберите тэги" data-dropdown-css-class="select2-purple" style="width: 100%;">
                                         @foreach($tags as $tag)
-                                            <option {{ is_array( old('tag_ids')) && in_array($tag->id, old('tag_ids')) ? ' selected' : '' }} value="{{ $tag->id }}">{{ $tag->title }}</option>
+                                            <option {{ is_array($post->tags->pluck('id')->toArray()) && in_array($tag->id, $post->tags->pluck('id')->toArray()) ? ' selected' : '' }} value="{{ $tag->id }}">{{ $tag->title }}</option>
                                         @endforeach
                                     </select>
                                 </div>
