99a3a3c3b440f10400d0636dd3e1b796beb3811a
sedusik <sedusova.tanya@yandex.ru>
2024-04-03 06:40:38 +0300

    add post updates(partly)

--- resources/views/admin/post/edit.blade.php
+++ resources/views/admin/post/edit.blade.php
@@ -30,14 +30,75 @@
                         <form action=" {{ route('admin.post.update', $post->id) }}" method="POST" class="w-25">
                             @csrf
                             @method('PATCH')
-                            <div class="form-group">
+                            <div class="form-group w-25">
                                 <input type="text" class="form-control" name="title" placeholder="Название поста"
-                                value=" {{ $post->title }} ">
+                                       value=" {{ old('title') }}"
+                                >
                                 @error('title')
-                                    <div class="text-danger">Это поле необходимо заполнить</div>
+                                <div class="text-danger">Это поле необходимо заполнить</div>
                                 @enderror
                             </div>
-                            <input type="submit" class="btn btn-primary" value="Обновить">
+                            <div class="form-group">
+                                <textarea id="summernote" name="content">
+                                    {{ old('content') }}
+                                </textarea>
+                                @error('content')
+                                <div class="text-danger">Это поле необходимо заполнить</div>
+                                @enderror
+                            </div>
+                            <div class="form-group w-50">
+                                <label for="exampleInputFile">Добавить превью</label>
+                                <div class="input-group">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" name="preview_image">
+                                        <label class="custom-file-label">Выберите изображение</label>
+                                    </div>
+                                    <div class="input-group-append">
+                                        <span class="input-group-text">Загрузка</span>
+                                    </div>
+                                </div>
+                                @error('preview_image')
+                                <div class="text-danger">Это поле необходимо заполнить</div>
+                                @enderror
+                            </div>
+                            <div class="form-group w-50">
+                                <label for="exampleInputFile">Добавить главное изображение</label>
+                                <div class="input-group">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" name="main_image">
+                                        <label class="custom-file-label">Выберите изображение</label>
+                                    </div>
+                                    <div class="input-group-append">
+                                        <span class="input-group-text">Загрузка</span>
+                                    </div>
+                                </div>
+                                @error('main_image')
+                                <div class="text-danger">Это поле необходимо заполнить</div>
+                                @enderror
+                            </div>
+                            <div class="form-group w-50">
+                                <label>Выберите категорию</label>
+                                <select name="category_id" class="form-control">
+                                    @foreach($categories as $category)
+                                        <option value="{{ $category->id }}"
+                                            {{ $category->id == old('category_id') ? ' selected' : '' }}
+                                        >{{ $category->title }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label>Тэги</label>
+                                <div class="select2-purple">
+                                    <select class="select2" name="tag_ids[]" multiple="multiple" data-placeholder="Выберите тэги" data-dropdown-css-class="select2-purple" style="width: 100%;">
+                                        @foreach($tags as $tag)
+                                            <option {{ is_array( old('tag_ids')) && in_array($tag->id, old('tag_ids')) ? ' selected' : '' }} value="{{ $tag->id }}">{{ $tag->title }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" class="btn btn-primary" value="Обновить">
+                            </div>
                         </form>
                     </div>
                     <!-- ./col -->
