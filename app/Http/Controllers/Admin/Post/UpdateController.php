1106caf3876b8edea1bd4001c19ccf683395bb37
sedusik <sedusova.tanya@yandex.ru>
2024-04-04 04:33:38 +0300

    refactoring the creation and updating of posts

--- app/Http/Controllers/Admin/Post/UpdateController.php
+++ app/Http/Controllers/Admin/Post/UpdateController.php
@@ -9,24 +9,12 @@ use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
-class UpdateController extends Controller
+class UpdateController extends BaseController
 {
     public function __invoke(UpdateRequest $request, Post $post)
     {
         $data = $request->validated();
-        $tagIds = $data['tag_ids'];
-        unset($data['tag_ids']);
-
-        if( array_key_exists('preview_image',$data)){
-            $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
-        }
-        if( array_key_exists('main_image',$data)){
-            $data['main_image'] = Storage::disk('public')->put('/images', $data['main_image']);
-        }
-
-        $post = Post::firstOrCreate($data);
-        $post->update($data);
-        $post->tags()->sync($tagIds);
+        $post = $this->service->update($data, $post);
         return view('admin.post.show', compact('post'));
     }
 }
