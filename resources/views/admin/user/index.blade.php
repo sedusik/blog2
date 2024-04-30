f04d0bab7ef6356fc5530531be803d4545020ada
sedusik <sedusova.tanya@yandex.ru>
2024-04-30 16:51:57 +0300

    refactor: change design admin panel (part 2)

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -12,7 +12,7 @@
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="{{ route('admin.main.index') }}">Главное меню</a></li>
+                            <li class="breadcrumb-item"><a href="{{ route('admin.main.index') }}">Панель админа</a></li>
                             <li class="breadcrumb-item active">Пользователи</li>
                         </ol>
                     </div><!-- /.col -->
